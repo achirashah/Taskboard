b1c2361e9ba99352c3c8f2df2bf5860bf3acb3a7
achirashah <shah.ac@husky.neu.edu>
2019-04-22 23:19:32 -0400

    init work on spring security

---
+++ taskboard/src/main/java/edu/neu/shah/taskboard/TaskboardConfig.java
@@ -0,0 +1,12 @@
+package edu.neu.shah.taskboard;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@EnableWebSecurity
+public class TaskboardConfig {
+
+}
